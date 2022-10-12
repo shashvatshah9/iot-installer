ad13fb32551891c4e75cc8c957aaaa9071d9d141
shashvat shah <shashvatshah9@gmail.com>
2022-10-13 04:07:04 +0530

    Update iot-installer.sh
    add changes for unix

--- iot-installer.sh
+++ iot-installer.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 run_mac(){
+    # free port 3000 & 4000
+    kill -9 $( lsof -i:3000 -t)
+    kill -9 $( lsof -i:4000 -t)
+    dir=~/iot_inspector
+
     if test ! $(which brew); then 
         echo "Installing homebrew..." 
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
@@ -27,9 +32,60 @@ run_mac(){
         ruby -e "$(brew install git)"
     fi
 
-    if [ ! -d "~/iot_inspector" ]; then
-        echo "$DIRECTORY does exist."
-        git clone https://github.com/nyu-mlab/iot-inspector-client.git ~/iot_inspector
+    if [ ! -d "$dir" ]; then
+        echo "$dir does not exist."
+        git clone https://github.com/nyu-mlab/iot-inspector-client.git $dir
+        cd ~/iot_inspector
+        git checkout cr-dev
+    fi
+
+    ## run python driver
+    cd $dir
+    pip3 install -r requirements.txt
+    ## pending run python driver TODO
+
+    ## run node scripts
+    cd ~/iot_inspector/ui/default
+    yarn install:all
+    yarn clean:db
+    yarn prisma:generate
+    yarn mockdb > /dev/null &
+    yarn dev > /dev/null &
+
+    echo "app running on http://localhost:3000"
+    open -a "Google Chrome" http://localhost:3000
+}
+
+run_linux(){
+ # free port 3000 & 4000
+    kill -9 $( lsof -i:3000 -t)
+    kill -9 $( lsof -i:4000 -t)
+    dir=~/iot_inspector
+
+    if test ! $(which python3); then
+        sudo apt-get update
+        echo "Installing python3..."
+        ruby -e "$(sudo apt-get install python3.9)"
+    fi
+
+    if test ! $(which node); then
+        echo "Installing nodejs..."
+        ruby -e "$(sudo apt-get install nodejs)"
+    fi
+
+    if test ! $(which yarn); then
+        echo "Intalling yarn..."
+        npm install --global yarn
+    fi
+
+    if test ! $(which git); then
+        echo "Installing git..."
+        ruby -e "$(sudo apt-get install git)"
+    fi
+
+    if [ ! -d "$dir" ]; then
+        echo "$dir does not exist."
+        git clone https://github.com/nyu-mlab/iot-inspector-client.git $dir
         cd ~/iot_inspector
         git checkout cr-dev
     fi
@@ -45,9 +101,12 @@ run_mac(){
     yarn clean:db
     yarn prisma:generate
     yarn mockdb > /dev/null &
-    yarn dev &
+    yarn dev > /dev/null &
+
+    echo "app running on http://localhost:3000"
+    google-chrome http://localhost:3000
 }
 
-# curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
 
 run_mac
